330ab16071d7ad46f432855bc65a7f84a05b48e2
tomiiha <52747351+tomiiha@users.noreply.github.com>
2020-05-24 16:19:55 +0100

    Update fb_parser.py

--- fb_parser.py
+++ fb_parser.py
@@ -1,41 +1,47 @@
 import json
 import os
 
+# Dump lists
 sender_list = []
 data_files = []
 results = {}
 
-# Get files first for batch processing
+# Get files in folder first for batch processing.
 file_list = os.listdir()
 for y in file_list:
     if y[-5:] == '.json':
         data_files.append(y)
 
-# Prog capture names in message group
-def read_mess(file_one):
-    data_file = json.load(open(file_one, 'r'))
-    for p_name in data_file['participants']:
-        add_name = p_name['name']
-        if add_name not in sender_list:
-            sender_list.append(add_name)
+# Prog capture names in message group.
+def read_mess(data_files):
+    for dt_file in data_files:
+        data_file = json.load(open(dt_file, 'r'))
+        for p_name in data_file['participants']:
+            add_name = p_name['name']
+            if add_name not in sender_list:
+                sender_list.append(add_name)
     return sender_list
 
-def mess_count(data_files, sender_list):
-    count = 0
+# 'mess_count' to capture total messages sent over time.
+def message_data(data_files, sender_list):
+    mess_count = 0
+    tot_words = 0
     for name in sender_list:
         for parse_file in data_files:
             dt_file = json.load(open(parse_file, 'r'))
             for msg in dt_file['messages']:
                 if msg['sender_name'] == name:
-                    count += 1
-        results[name] = count
-    count = 0
+                    mess_count += 1
+                    #tot_words += len(msg['content'].split())
+        results[name] = mess_count, (tot_words / mess_count)
+    mess_count = 0
+    tot_words = 0
     return results
-     
-# Cap names, then count messages per name
-for z in data_files:
-    read_mess(z)
-mess_count(data_files, sender_list)
 
+# Processing
+read_mess(data_files)
+message_data(data_files, sender_list)
+
+# Outputs
 sort_res = sorted(results.items(), key=lambda x: x[1], reverse=True)
 print(sort_res)
